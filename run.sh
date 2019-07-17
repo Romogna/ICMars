1d4c2c185da975e639cccfaf5f584cf7bbdcb17e
Romogna <alderon13@gmail.com>
2019-07-17 15:45:35 -0600

    Able to find Arduino port

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ echo "You can't prove anything!"
 
 export HOSTNAME="$HOSTNAME"
 export T1="Marvel"
+export ARDUINO_PORT="$(./get_arduino_port.sh)"
 
 source devel/setup.bash
 
+echo "Arduino on port $ARDUINO_PORT"
 echo "$T1 launching on $HOSTNAME"
 
 if [ "Marvel" = "$T1" ]; then
