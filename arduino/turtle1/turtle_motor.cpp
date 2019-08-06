21b202e5bea6afc940a765c8cf9cc80678df30b4
emland <emland@sipi-i-c-mars.org>
2019-08-06 15:58:12 -0600

    added comments to area for skidsteer

--- arduino/turtle1/turtle_motor.cpp
+++ arduino/turtle1/turtle_motor.cpp
@@ -20,10 +20,10 @@ turtle_motor::turtle_motor(ros::NodeHandle *nodeHandle)
 	DIR_FWD[1] = LOW;
 	DIR_REV[0] = LOW;
 	DIR_REV[1] = HIGH;
-  DIR_R[0] = HIGH;
-  DIR_R[1] = HIGH;
-  DIR_L[0] = LOW;
-  DIR_L[1] = LOW;
+	DIR_R[0] = HIGH;
+	DIR_R[1] = HIGH;
+	DIR_L[0] = LOW;
+	DIR_L[1] = LOW;
 	commandTimeoutMs = 50;
 	loopMs = 50;
 	efforts[0] = efforts[1] = 0;
@@ -40,33 +40,34 @@ void turtle_motor::motorMessageCb( const geometry_msgs::Twist& cmd_msg){
 	commandTimeout = commandTimeoutMs;
 }
 
-void turtle_motor::writeMotors(void) 
+void turtle_motor::writeMotors(void)
 {
 	if ((efforts[0] == 0) & (efforts[1] == 0)) {
 		for(int i=0;i<2;++i) {
-			//		digitalWrite(brake[i], HIGH);
+			//digitalWrite(brake[i], HIGH);
 			analogWrite(pwm[i], 0);
 		}
 	} else {
 		for(int i=0;i<2;++i) {
-			//		digitalWrite(brake[i], LOW);
+			//digitalWrite(brake[i], LOW);
 			if(efforts[0] < 0) {
 				digitalWrite(dir[i], DIR_REV[i]);
 				// the 2.55 is to scale 100% to full scale on pwm (255)
 				analogWrite(pwm[i], -(int)((float)efforts[0]*2.55));
-			} 
+			}
 			else if(efforts[0] > 0) {
 				digitalWrite(dir[i], DIR_FWD[i]);
 				analogWrite(pwm[i], (int)((float)efforts[0]*2.55));
-			}
-      else if(efforts[1] < 0) {
-        digitalWrite(dir[i], DIR_L[i]);
-        analogWrite(pwm[i], -(int)((float)efforts[1]*2.55));
-      }
-      else if(efforts[1] > 0) {
-        digitalWrite(dir[i], DIR_R[i]);
-        analogWrite(pwm[i], (int)((float)efforts[1]*2.55));
-      }
+      			}
+			//Needs to enable skidsteering for low effort turns
+      			else if(efforts[1] < 0) { //left
+        			digitalWrite(dir[i], DIR_L[i]);
+      				analogWrite(pwm[i], -(int)((float)efforts[1]*2.55));
+      			}
+      			else if(efforts[1] > 0) { //right
+      				digitalWrite(dir[i], DIR_R[i]);
+      				analogWrite(pwm[i], (int)((float)efforts[1]*2.55));
+      			}
  		}
 	}
 }
@@ -77,7 +78,7 @@ void turtle_motor::setup(void)
 		pinMode(dir[i], OUTPUT);
 		pinMode(pwm[i], OUTPUT);
 		analogWrite(pwm[i], 100);
-		//		pinMode(brake[i], OUTPUT);
+		//pinMode(brake[i], OUTPUT);
 	}
 	status_publisher = new ros::Publisher("motor_status", &status_msg);
 	nh->advertise(*status_publisher);
